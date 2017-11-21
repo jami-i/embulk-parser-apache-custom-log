adeaa9a6219e434c337e73c6105c4a9177776166
Osamu Ishikawa <o-ishikawa@nri-net.com>
2017-11-21 18:21:55 +0900

    fix unit test

--- src/test/java/org/embulk/parser/apache/log/LogFormatsTest.java
+++ src/test/java/org/embulk/parser/apache/log/LogFormatsTest.java
@@ -1,14 +1,20 @@
 package org.embulk.parser.apache.log;
 
-import junit.framework.TestCase;
+import org.embulk.EmbulkTestRuntime;
 import org.embulk.spi.time.TimestampParser;
 import org.joda.time.DateTimeZone;
 import org.jruby.embed.ScriptingContainer;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.util.regex.Pattern;
 
-public class LogFormatsTest extends TestCase {
+public class LogFormatsTest {
 
+    @Rule
+    public EmbulkTestRuntime runtime = new EmbulkTestRuntime();
+
+    @Test
     public void testLogFormat2Regexp() throws Exception {
 
         String format = "%!100<v %!100,200<v %100,200,300>v %!100,200,300>{hogeHoge}v %v %{X-Forwarded-For}i %t %{%D}t %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %I %O %D";
